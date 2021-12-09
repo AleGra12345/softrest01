9bae34e9c08aa470ba0911a8e37f5f52def9a2db
ceda-dev-01 <l.lezzerini@coopceda.it>
2021-12-09 13:01:25 +0100

    Chiamata rest funzionante

--- rest01/src/app/app.component.ts
+++ rest01/src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'rest01';
+
+  righe: Riga[] = [];
+
+  // 1) Angular instanzia un oggetto di tipi HttpClient e lo passa come argomento
+  // 2) definite una proprietà http:
+  //    http: HttpClient;
+  // 3) valorizzate this.http = http;
+  constructor(private http: HttpClient) { }
+
+  carica() {
+    let oss: Observable<Riga[]> = this.http
+      .get<Riga[]>("https://jsonplaceholder.typicode.com/posts");
+      oss.subscribe(r => {
+        this.righe = r;
+      });
+  }
+}
+
+export class Riga {
+  userId: number = 0;
+  id: number = 0;
+  title: string = "";
+  body: string = "";
 }
